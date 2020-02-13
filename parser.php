6dc4cd19d3b567eb6b33ce58b4d71b84418d351c
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-02-13 21:16:38 +0100

    Added constants for instruction, types and error codes. Then added parser method to Parser class.

--- parser.php
+++ parser.php
@@ -3,19 +3,59 @@
     class Instructions
     {
         const INSTRUCTIONS = array(
-            "write" => ["string"]
+            "MOVE" => [Types::VARIABLE, Types::SYMBOL],
+            "CREATEFRAME" => [],
+            "PUSHFRAME" => [],
+            "POPFRAME" => [],
+            "DEFVAR" => [Types::VARIABLE],
+            "CALL" => [Types::LABEL],
+            "RETURN" => [],
+            "PUSHS" => [Types::SYMBOL],
+            "POPS" => [Types::VAR],
+            "ADD" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "SUB" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "MUL" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "IDIV" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "LT" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "GT" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "EQ" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "AND" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "OR" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "NOT" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "INT2CHAR" => [Types::VARIABLE, Types::SYMBOL],
+            "STR2INT" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "READ" => [Types::VARIABLE, Types::TYPE],
+            "WRITE" => [Types::SYMBOL],
+            "CONCAT" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "STRLEN" => [Types::VARIABLE, Types::SYMBOL],
+            "GETCHAR" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "SETCHAR" => [Types::VARIABLE, Types::SYMBOL, Types::SYMBOL],
+            "TYPE" => [Types::VARIABLE, Types::SYMBOL],
+            "LABEL" => [Types::LABEL],
+            "JUMP" => [Types::LABEL],
+            "JUMPIFEQ" => [Types::LABEL, Types::SYMBOL, Types::SYMBOL],
+            "JUMPIFNEQ" => [Types::LABEL, Types::SYMBOL, Types::SYMBOL],
+            "EXIT" => [Types::SYMBOL],
+            "DPRINT" => [Types::SYMBOL],
+            "BREAK" => []
         );
 
     }
 
     class Errors
     {
-
+        const BAD_ARGUMENT = 10;
+        const HEADER_ERR = 21;
+        const INSTRUCTION_ERR = 22;
+        const LEX_OR_SYNTAX_ERR = 23;
     }
 
     class Types
     {
-
+        const VARIABLE = 1;
+        const LABEL = 2;
+        const SYMBOL = 3;
+        const TYPE = 4;
     }
 
     class Regex
@@ -58,6 +98,18 @@
             $this->syntaxChecker = $syntaxChecker;
             $this->xmlGenerator = $xmlGenerator;
             $this->fileManager = $fileManager;
+        }
+
+        public function parse()
+        {
+            $token = $this->lexicalChecker->getNextToken();
+            if(!$this->syntaxChecker->checkFirstToken($token)) {
+                throw new Exception("Bad Header", Errors::HEADER_ERR);
+            }
+            while(!feof($this->fileManager->getFile()))
+            {
+                $token = $this->lexicalChecker->getNextToken();
+            }
         }
     }
 
@@ -132,8 +184,15 @@
 
     class SyntaxChecker
     {
-        public function checkNumOfParameters($opCode, $numberOfParameters) {
+        public function checkNumOfParameters($opCode, $numberOfParameters)
+        {
             return count(Instructions::INSTRUCTIONS[$opCode]) == $numberOfParameters;
+        }
+
+        public function checkFirstToken($token)
+        {
+            $token = mb_strtolower($token[0]);
+            return strcmp(".ippcode20", $token) == 0;
         }
     }
 
@@ -179,8 +238,15 @@
 
     $fileManager = new FileManager(STDIN);
     $lexicalChecker = new LexicalChecker($fileManager);
-
-    while(!feof($fileManager->getFile())){
-        print_r($lexicalChecker->getNextToken());
+    $syntaxChecker = new SyntaxChecker();
+    $xmlGenerator = new XmlGenerator();
+    $parser = new Parser($lexicalChecker, $syntaxChecker, $xmlGenerator, $fileManager);
+    try {
+        $parser->parse();
+    } catch (Exception $e) {
+        exit($e->getCode());
     }
+
+
+
 
