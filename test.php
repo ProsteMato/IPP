fd6ccd15c367a833457ca0304b707d9327cfecd3
ProsteMato <xkocim05@stud.fit.vutbr.cz>
2020-02-22 01:30:17 +0100

    added instance of Tester and added new exception

--- test.php
+++ test.php
@@ -6,11 +6,14 @@
     require_once "./Classes/TestClasses/HtmlGenerator.php";
     require_once "./Classes/TestClasses/ResultGenerator.php";
     require_once "./Classes/TestClasses/OptionParser.php";
-    require_once "Classes/ExceptionClasses/NotExistingFileException.php";
-    require_once "Classes/ExceptionClasses/BadArgumentCombinationException.php";
-    require_once "Classes/ExceptionClasses/RedefinitionOfArgumentException.php";
-    require_once "Classes/ExceptionClasses/RequiredValueException.php";
-    require_once "Classes/ExceptionClasses/UndefinedArgumentException.php";
+    require_once "./Classes/TestClasses/Tester.php";
+    require_once "./Classes/ExceptionClasses/ArgumentException.php";
+    require_once "./Classes/ExceptionClasses/NotExistingFileException.php";
+    require_once "./Classes/ExceptionClasses/BadArgumentCombinationException.php";
+    require_once "./Classes/ExceptionClasses/RedefinitionOfArgumentException.php";
+    require_once "./Classes/ExceptionClasses/RequiredValueException.php";
+    require_once "./Classes/ExceptionClasses/UndefinedArgumentException.php";
+    require_once "./Classes/ExceptionClasses/NotInstanceOfException.php";
 
     function optionsChecker(&$options) {
         if (key_exists("help", $options) && count($options) > 1) {
@@ -27,11 +30,11 @@
         if (!key_exists("directory", $options)) {
             $options["directory"] = "./";
         }
-        if (!key_exists("parse-script", $options) && !key_exists("int-only", $options)) {
-            $options["parse-script"] = "./";
+        if (!key_exists("parse-script", $options)) {
+            $options["parse-script"] = "./parser.php";
         }
-        if (!key_exists("int-script", $options) && !key_exists("parse-only", $options)) {
-            $options["int-script"] = "./";
+        if (!key_exists("int-script", $options)) {
+            $options["int-script"] = "./interpret.py";
         }
         if (!key_exists("jexamxml", $options)) {
             $options["jexamxml"] = "/pub/courses/ipp/jexamxml/jexamxml.jar";
@@ -62,6 +65,11 @@
         }
         $fileAdmin = new FileAdministrator($options["directory"], key_exists("recursive", $options));
         $testSuite = $fileAdmin->getTestSuites();
+        $tester = new Tester(
+            key_exists("parse-only", $options), key_exists("int-only", $options),
+            $options["parse-script"], $options["int-script"], $options["jexamxml"]
+        );
+        $tester->runTests($testSuite);
         $results = new ResultGenerator();
         $results->generateResults($testSuite);
     } catch (NotExistingFileException $e){
@@ -70,6 +78,9 @@
     } catch (ArgumentException $e) {
         error_log($e->getMessage());
         exit(Errors::BAD_ARGUMENT);
+    } catch (NotInstanceOfException $e) {
+        error_log($e->getMessage());
+        exit(Errors::INTERNAL_ERROR);
     }
 
 
